8102178a3fe181ac496cb937dbc926e33d7d4916
Александр Небайкин <iamarokk@gmail.com>
2019-06-21 19:56:46 +0300

    Add project files.

---
+++ Picture/FormResize.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Picture
+{
+    partial class PhotoSharp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhotoSharp));
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.buttonOpenPicture = new System.Windows.Forms.Button();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelScale = new System.Windows.Forms.Label();
+            this.trackBarScale = new System.Windows.Forms.TrackBar();
+            this.comboBoxMethod = new System.Windows.Forms.ComboBox();
+            this.labelMethod = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture.Location = new System.Drawing.Point(12, 27);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(978, 494);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // buttonOpenPicture
+            // 
+            this.buttonOpenPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpenPicture.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpenPicture.Location = new System.Drawing.Point(996, 27);
+            this.buttonOpenPicture.Name = "buttonOpenPicture";
+            this.buttonOpenPicture.Size = new System.Drawing.Size(183, 60);
+            this.buttonOpenPicture.TabIndex = 1;
+            this.buttonOpenPicture.Text = "Выбрать картинку";
+            this.buttonOpenPicture.UseVisualStyleBackColor = true;
+            this.buttonOpenPicture.Click += new System.EventHandler(this.buttonOpenPicture_Click);
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFileName.Location = new System.Drawing.Point(1000, 93);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.ReadOnly = true;
+            this.textBoxFileName.Size = new System.Drawing.Size(179, 20);
+            this.textBoxFileName.TabIndex = 2;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(997, 462);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(183, 59);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Изменить картинку";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelScale
+            // 
+            this.labelScale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelScale.AutoSize = true;
+            this.labelScale.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScale.Location = new System.Drawing.Point(996, 135);
+            this.labelScale.Name = "labelScale";
+            this.labelScale.Size = new System.Drawing.Size(79, 19);
+            this.labelScale.TabIndex = 5;
+            this.labelScale.Text = "Масштаб:";
+            // 
+            // trackBarScale
+            // 
+            this.trackBarScale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarScale.LargeChange = 10;
+            this.trackBarScale.Location = new System.Drawing.Point(996, 165);
+            this.trackBarScale.Maximum = 5000;
+            this.trackBarScale.Minimum = 10;
+            this.trackBarScale.Name = "trackBarScale";
+            this.trackBarScale.Size = new System.Drawing.Size(184, 45);
+            this.trackBarScale.TabIndex = 6;
+            this.trackBarScale.TickFrequency = 100;
+            this.trackBarScale.Value = 100;
+            this.trackBarScale.Scroll += new System.EventHandler(this.trackBarScale_Scroll);
+            // 
+            // comboBoxMethod
+            // 
+            this.comboBoxMethod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMethod.FormattingEnabled = true;
+            this.comboBoxMethod.Items.AddRange(new object[] {
+            "Метод ближайшего соседа",
+            "Билинейная интерполяция"});
+            this.comboBoxMethod.Location = new System.Drawing.Point(996, 238);
+            this.comboBoxMethod.Name = "comboBoxMethod";
+            this.comboBoxMethod.Size = new System.Drawing.Size(183, 21);
+            this.comboBoxMethod.TabIndex = 7;
+            // 
+            // labelMethod
+            // 
+            this.labelMethod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMethod.AutoSize = true;
+            this.labelMethod.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMethod.Location = new System.Drawing.Point(997, 213);
+            this.labelMethod.Name = "labelMethod";
+            this.labelMethod.Size = new System.Drawing.Size(81, 19);
+            this.labelMethod.TabIndex = 8;
+            this.labelMethod.Text = "Алгоритм";
+            // 
+            // PhotoSharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 533);
+            this.Controls.Add(this.labelMethod);
+            this.Controls.Add(this.comboBoxMethod);
+            this.Controls.Add(this.trackBarScale);
+            this.Controls.Add(this.labelScale);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.buttonOpenPicture);
+            this.Controls.Add(this.picture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PhotoSharp";
+            this.Text = "Измения размера картинок";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button buttonOpenPicture;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelScale;
+        private System.Windows.Forms.TrackBar trackBarScale;
+        private System.Windows.Forms.ComboBox comboBoxMethod;
+        private System.Windows.Forms.Label labelMethod;
+    }
+}
+
---
+++ Picture/FormResize.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture
+{
+    public partial class PhotoSharp : Form
+    {
+        Bitmap bmp;
+        string text_scale;
+
+        public PhotoSharp()
+        {
+            InitializeComponent();
+            LoadPicture("aperture-25.jpg");
+            text_scale = labelScale.Text;
+            TextChanges();
+        }
+
+        private void buttonOpenPicture_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            DialogResult result = ofd.ShowDialog();
+            LoadPicture(ofd.FileName);
+        }
+
+        public void LoadPicture(string filename)
+        {
+
+            try
+            {
+                bmp = new Bitmap(Image.FromFile(filename));
+                textBoxFileName.Text = filename;
+                picture.Image = bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBoxFileName.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBoxMethod.SelectedIndex == 0)
+                ResizeNearest();
+            else
+                if (comboBoxMethod.SelectedIndex == 1)
+                ResizeBilinear();
+        }
+
+        public void ResizeNearest()
+        {
+            float coeff = trackBarScale.Value / 100.0f;
+            int oldWidth = bmp.Width;
+            int oldHeight = bmp.Height;
+            int newWidth = Convert.ToInt16(oldWidth * coeff);
+            int newHeight = Convert.ToInt16(oldHeight * coeff);
+
+            float coWidth = (float)(oldWidth - 1) / (float)(newWidth - 1);
+            float coHeigth = (float)(oldHeight - 1) / (float)(newHeight - 1);
+
+            Bitmap res = new Bitmap(newWidth, newHeight);
+            int x0, y0;
+            for (int y = 0; y < newHeight; y++)
+            {
+                y0 = Convert.ToInt16(y * coHeigth);
+                for (int x = 0; x < newWidth; x++)
+                {
+                    x0 = Convert.ToInt16(x * coWidth);
+                    Color pixel = bmp.GetPixel(x0, y0);
+                    res.SetPixel(x, y, pixel);
+                }
+            }
+            picture.Image = res;
+        }
+
+        public void ResizeBilinear()
+        {
+            float coeff = trackBarScale.Value / 100.0f;
+            int oldWidth = bmp.Width;
+            int oldHeight = bmp.Height;
+            int newWidth = Convert.ToInt16(oldWidth * coeff);
+            int newHeight = Convert.ToInt16(oldHeight * coeff);
+
+            float coWidth = (float)(oldWidth - 1) / (float)(newWidth - 1);
+            float coHeigth = (float)(oldHeight - 1) / (float)(newHeight - 1);
+
+            Bitmap res = new Bitmap(newWidth, newHeight);
+            float x, y;
+            int x1, y1;
+            int x2, y2;
+            for (int newY = 0; newY < newHeight; newY++)
+            {               
+                for (int newX = 0; newX < newWidth; newX++)
+                {
+                    x = newX * coWidth;
+                    y = newY * coHeigth;
+                    x1 = Convert.ToInt16(Math.Floor(x));
+                    y1 = Convert.ToInt16(Math.Floor(y));
+                    if (x1 > oldWidth - 2) x1 = oldWidth - 2;
+                    if (y1 > oldHeight - 2) y1 = oldHeight - 2;
+                    x2 = x1 + 1;
+                    y2 = y1 + 1;
+
+                    Color Q11 = bmp.GetPixel(x1, y1);
+                    Color Q12 = bmp.GetPixel(x1, y2);
+                    Color Q21 = bmp.GetPixel(x2, y1);
+                    Color Q22 = bmp.GetPixel(x2, y2);
+                    int R = BilinearValue(x, y, x1, y1, x2, y2, Q11.R, Q21.R, Q22.R);
+                    int G = BilinearValue(x, y, x1, y1, x2, y2, Q11.G, Q21.G, Q22.G);
+                    int B = BilinearValue(x, y, x1, y1, x2, y2, Q11.B, Q21.B, Q22.B);
+                    Color pixel = Color.FromArgb(R, G, B);
+                    res.SetPixel(newX, newY, pixel);
+                }
+            }
+            picture.Image = res;
+        }
+
+        private static int BilinearValue(float x, float y, int x1, int y1, int x2, int y2, 
+            int Q11, int Q21, int Q22)
+        {
+            int R1;
+            int R2;
+            int P;
+            R1 = Convert.ToInt16((x2 - x1) * Q11 + (x - x1) * Q21);
+            R2 = Convert.ToInt16((x2 - x) * Q11 + (x - x1) * Q22);
+            P = Convert.ToInt16((y2 - y) * R1 + (y - y1) * R2);
+            if (P < 0) P = 0;
+            if (P > 255) P = 255;
+            return P;
+        }
+
+        private void TextChanges()
+        {
+            labelScale.Text = text_scale + " " + trackBarScale.Value.ToString() + "%";
+        }
+
+        private void trackBarScale_Scroll(object sender, EventArgs e)
+        {
+            TextChanges();
+        }
+
+        
+    }
+}
